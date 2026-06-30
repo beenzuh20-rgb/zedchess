9b8f8047bf4feb70cefdf197ed4d5f67fe279528
beenzuh20-rgb <beenzuh20@gmail.com>
2026-06-30 16:46:09 +0200

    Update app.py
    app.py

--- app.py
+++ app.py
@@ -823,5 +823,13 @@ def close_db(exception=None):
         db.close()
 
 
+import os
+
 if __name__ == "__main__":
-    socketio.run(app, debug=True, host="127.0.0.1", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(
+        app,
+        host="0.0.0.0",
+        port=port,
+        debug=False
+    )
